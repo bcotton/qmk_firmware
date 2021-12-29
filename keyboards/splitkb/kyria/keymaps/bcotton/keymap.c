3984f1a0da292be08bd9a5bfb6c9090d32100d2d
Bob Cotton <bob.cotton@gmail.com>
2021-12-29 12:07:37 -0700

    Added num layer

--- keyboards/splitkb/kyria/keymaps/bcotton/keymap.c
+++ keyboards/splitkb/kyria/keymaps/bcotton/keymap.c
@@ -17,9 +17,9 @@
 
 enum layers {
     _QWERTY = 0,
-    _NAV,
     _SYM,
-    _FUNCTION,
+    _NAV,
+    _NUM,
     _ADJUST,
 };
 
@@ -29,7 +29,7 @@ enum layers {
 
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
-#define FKEYS    MO(_FUNCTION)
+#define NUM      MO(_NUM)
 #define ADJUST   MO(_ADJUST)
 
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,             KC_W   ,           KC_E  ,            KC_R ,             KC_T ,                                                 KC_Y,   KC_U ,             KC_I ,             KC_O ,             KC_P ,                  KC_BSPC,
      TD(DANCE_0) , MT(MOD_LCTL, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LGUI, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                              KC_H,   MT(MOD_RSFT, KC_J),MT(MOD_RGUI, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RCTL, KC_SCOLON),KC_QUOT,
-     KC_LSFT , KC_Z ,             KC_X   ,           KC_C  ,            KC_V ,             KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC,          KC_N,   KC_M ,             TD(DANCE_1),       TD(DANCE_2),       TD(DANCE_3),            KC_RSFT,
+     KC_LSFT , KC_Z ,             KC_X   ,           KC_C  ,            KC_V ,             KC_B , KC_LBRC,KC_CAPS,    _______ , KC_RBRC,          KC_N,   KC_M ,             TD(DANCE_1),       TD(DANCE_2),       TD(DANCE_3),            KC_RSFT,
                                 _______ , KC_LGUI, NAV, KC_SPC , _______   ,                                          _______ , KC_BSPC , SYM, KC_RGUI, _______
     ),
 
@@ -83,17 +83,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        |  GUI |  Alt | Ctrl | Shift|      |                              | PgDn |  ←   |   ↓  |   →  | VolDn| Insert |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |ScLck |  |      |      | Pause|M Prev|M Play|M Next|VolMut| PrtSc  |
+ * |        |      |      |      |      |      |      |      |  |      |      |      |      |      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_NAV] = LAYOUT(
-      _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENTER, KC_INS,
-      _______, _______, _______, _______, _______, _______, _______, KC_SLCK, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
-                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+      _______, _______, _______, _______, _______, _______,                                     LGUI(KC_LEFT),  LALT(LGUI(KC_LEFT)),LALT(LGUI(KC_RIGHT)),RGUI(KC_RIGHT),  KC_VOLD, KC_VOLU,
+      _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,                                     KC_LEFT,        KC_DOWN,            KC_UP,               KC_RGHT,         KC_ENTER, KC_INS,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,_______,        _______,            _______,             _______,         _______,  _______,
+                                 _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______
     ),
 
 /*
@@ -116,27 +116,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
-
 /*
- * Function Layer: Function keys
+ * Num Layer: Numbers on a numpad
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |  F9  | F10  | F11  | F12  |      |                              |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |                              |      |  7   |  8   |  9   |      |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |  F5  |  F6  |  F7  |  F8  |      |                              |      | Shift| Ctrl |  Alt |  GUI |        |
+ * |        |  GUI |  Alt | Ctrl | Shift|      |                              |      |  4   |  5   |  6   | Ret  |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |  F1  |  F2  |  F3  |  F4  |      |      |      |  |      |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |      |      |  |      |      |   0  |  1   |  2   |  3   |  .   |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
-    [_FUNCTION] = LAYOUT(
-      _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, _______,                                     _______, _______, _______, _______, _______, _______,
-      _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , _______,                                     _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI, _______,
-      _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    [_NUM] = LAYOUT(
+      _______, _______, _______, _______, _______, _______,                                     _______ ,KC_7 ,KC_8, KC_9, _______, _______,
+     _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,                                     _______, KC_4, KC_5, KC_6, KC_ENTER, _______,
+     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_1, KC_2, KC_3, KC_DOT, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
+
 
 /*
  * Adjust Layer: Default layer settings, RGB
