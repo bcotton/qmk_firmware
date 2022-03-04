72f4a067f91f2595fadd2885254076397cd44e2b
Bob Cotton <bob.cotton@gmail.com>
2022-03-04 10:20:42 -0700

    First workring set for the Iris

--- keyboards/keebio/iris/keymaps/bcotton/config.h
+++ keyboards/keebio/iris/keymaps/bcotton/config.h
@@ -20,4 +20,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 
-#define TAPPING_TERM 200
+// Lets you roll mod-tap keys
+#define IGNORE_MOD_TAP_INTERRUPT
